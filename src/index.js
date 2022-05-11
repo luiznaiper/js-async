a48a08d0f6f3177eeb15f62529b531ac12119a5d
luiznaiper <luiz_naiper@hotmail.com>
2022-05-11 07:07:22 -0300

    Added script to package.json, doing trials with callback function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('object');
+const sum = (num1, num2) => {
+    return num1 + num2
+}
+
+const calc = (num1, num2, callback) => {
+    return callback(num1, num2)
+}
+
+console.log(calc(2,2, sum))
+
+const date = (callback) => {
+    console.log(new Date)
+    setTimeout(() => {
+        const date2 = new Date
+        callback(date2)
+    }, 2000)
+}
+
+const printDate = (dateNow) =>{
+    console.log(dateNow)
+}
+
+date(printDate)
